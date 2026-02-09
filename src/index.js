482d1d7cdcca3c79bf2cc79ea2dcc6ee1b0d3bad
rajneesh <RajneeshOjha0@gmail.com>
2026-02-09 12:28:07 +0530

    feat: set up initial Express server with API routing and a basic info endpoint.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { PORT } = require("./config");
-
+const apiRouter = require('./routes/index')
 const app = express();
-
+app.use(express.json());
+app.use('/api', apiRouter)
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
